291bea45a55f0abf80035a801bd37669302eea9b
Gunashree2005 <115690688+Gunashree2005@users.noreply.github.com>
2023-04-18 14:22:51 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,22 @@
-
+function input()
+{
+  let a=parsenInt(prompt("enter the number"));
+  let b=parasenInt("enter the another number));
+   return [a,b];
+}
+function sub(a,b)
+{
+  return a-b;
+}
+function output(a,b,c)
+{
+  console.log("%d - %d is %d",a,b,c);
+}
+function main()
+{
+  let a,b;
+  [a,b]=input( );
+  let c = sub(a,b)
+  output(a,b,c);
+}
+main();
